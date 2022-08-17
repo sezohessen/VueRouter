0657b9db58557aad608131a1666e861c550919b5
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-17 22:36:05 +0200

    File structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav>
+  <nav id="nav">
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'about'}">About</router-link>
+    <router-link :to="{ name: 'Jobs'}">Jobs</router-link>
   </nav>
   <router-view/>
 </template>
@@ -13,18 +14,21 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 960px;
+  margin: 0 auto;
 }
-
-nav {
+#nav {
   padding: 30px;
 }
-
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+#nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
+import Jobs from '../views/Jobs/Jobs.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
   }
 ]
 
