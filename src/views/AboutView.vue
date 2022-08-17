8f54e314845d58d4fbabbb95f501f28c05c95856
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-17 22:24:40 +0200

    Router Links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores veniam id architecto animi cupiditate, et eius ipsa voluptate dolorum, quis aliquid molestias modi laborum? Provident animi commodi ab cupiditate, voluptate quis natus dolorum fugiat veniam mollitia impedit odit optio ipsam. Iusto nobis pariatur harum molestias doloremque sequi cumque architecto accusantium a. Sed exercitationem deserunt minus culpa voluptas eum eligendi enim aspernatur hic quisquam nulla laudantium rem dolorem suscipit iusto impedit labore voluptate, ab, facilis distinctio? Quo optio modi esse adipisci temporibus vitae tenetur velit iste, alias omnis error porro. Enim, exercitationem? Vero aperiam laboriosam dolores unde laborum dicta quisquam accusamus.</p>
+    
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, doloribus id, quo tempora ratione autem, praesentium repellat laboriosam rem recusandae maiores eaque commodi? Rerum quia quae possimus animi suscipit laudantium.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, doloribus id, quo tempora ratione autem, praesentium repellat laboriosam rem recusandae maiores eaque commodi? Rerum quia quae possimus animi suscipit laudantium.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, doloribus id, quo tempora ratione autem, praesentium repellat laboriosam rem recusandae maiores eaque commodi? Rerum quia quae possimus animi suscipit laudantium.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
