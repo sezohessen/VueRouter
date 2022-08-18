756c882e7ba8895a0592f3c695e75a516f590414
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-18 21:12:39 +0200

    Fetching data from json server

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,16 +1,24 @@
 <template>
-  <h1>Job details page</h1>
+  <h1>{{ job.title }}</h1>
   <p>The job ID is : {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
-    props:['id']
-    /* data() {
+    props:['id'],
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(response => response.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    },
+    data() {
         return {
-            id: this.$route.params.id
+            /* id: this.$route.params.id */
+            job:null
         }
-    }, */
+    },
 }
 </script>
 
